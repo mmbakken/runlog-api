c861653f9ea3f93574e9e3ed860e0f0c35daf08b
Matt Bakken <mmbakken@gmail.com>
2020-12-16 16:36:32 -0700

    Added open CORS policy; added gitignore

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
 import express from 'express'
+import cors from 'cors'
 
 const app = express()
-const port = 4000
+const port = 80
+
+app.use(cors)
 
 app.get('/', (req, res) => {
   res.send('Runlog API :)')
 })
 
 app.listen(port, () => {
-  console.log(`Magic happens at http://localhost:${port}`)
+  console.log(`Magic happens at port ${port}`)
 })
