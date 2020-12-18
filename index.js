31f46cb09133ed99ec9fcce2124344078296a8b2
Matt Bakken <mmbakken@gmail.com>
2020-12-17 21:13:16 -0700

    Turning off cors on node b/c I added it to nginx instead

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 import express from 'express'
-import cors from 'cors'
+//import cors from 'cors'
 
 const app = express()
 const port = 4000
 
-app.use(cors())
+//app.use(cors())
 
 app.get('/api/v1', (req, res) => {
   res.send('Runlog API v1 ✌️')
