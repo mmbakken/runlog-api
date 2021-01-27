db0b644ef899f1a53dcf7e51092d26a91bdeae5f
Matt Bakken <mmbakken@gmail.com>
2021-01-26 20:52:35 -0700

    Created the Run collection schema to use for importing Strava activities to Runlog. Also saved an example of a simple Strava activity which is NOT the detailed version that can be retrieved in a separate call. Those fields can come later.

---
+++ db/runSchema.js
@@ -0,0 +1,33 @@
+import mongoose from 'mongoose'
+
+// This collection holds run documents that represent run activities on a user's
+// third-party linked accounts like Strava, Fitbit, etc. 
+const runSchema = new mongoose.Schema({
+  userId: mongoose.ObjectId, // Runlog: 'user._id'
+  name: String, // User can edit this field in Strava
+  startDate: Date,
+  time: Number, // Strava: 'moving_time'
+  distance: Number, // In meters
+  averageSpeed: Number, // In meters per second
+  totalElevationGain: Number, // In feet
+  hasHeartRate: Boolean,
+  averageHeartRate: Number,
+  maxHeartRate: Number,
+  stravaActivityId: String, // Strava: 'id'
+  stravaExternalId: String, // Strava: 'external_id', from Garmin or Fitbit, e.g.
+
+  // TODO: Retrieve these fields in a separate call, not part of the bulk import
+  // Fields that have to be retrieved from Strava's get activity details endpoint
+  // GET /activity/:id
+//   calories: Number, // TODO: Is this used for run activities?
+//   description: String, // From Strava; Need to decide if "comments" is the same
+//   laps: [], // TODO: lap data might get big; think about this more.
+// 
+//   // Runlog-added fields
+//   comments: String,
+//   strengthComments: String,
+//   didStretch: Boolean,
+//   didIce: Boolean,
+})
+
+export default runSchema
