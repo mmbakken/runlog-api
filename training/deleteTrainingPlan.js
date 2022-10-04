a3192467bdd24e900a54250c8e1fadd1b4aac6aa
Matthew Bakken <mmbakken@gmail.com>
2022-10-04 01:32:46 -0600

    DELETE /training/:id now works

--- training/deleteTrainingPlan.js
+++ training/deleteTrainingPlan.js
@@ -1,9 +1,16 @@
-// import TrainingModel from '../db/TrainingModel.js'
+import TrainingModel from '../db/TrainingModel.js'
 
 // Returns the logged in user's training plans from the database.
 const deleteTrainingPlan = async (req, res) => {
-  // TODO: implement this function
-  return res.status(501).json({ error: 'Unable to delete training plan: route not implemented.'})
+  let deletedPlan
+
+  try {
+    deletedPlan = await TrainingModel.deleteOne({_id: req.params.id})
+  } catch (e) {
+    return res.status(501).json({ error: 'Unable to delete training plan'})
+  }
+
+  return res.status(200).json(deletedPlan)
 }
 
 export default deleteTrainingPlan
