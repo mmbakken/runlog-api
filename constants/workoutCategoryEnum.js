ca33264aa4594ee692194c1c9d5a87ee2bcc5feb
Matthew Bakken <mmbakken@gmail.com>
2022-10-05 16:56:33 -0600

    Updated workout category enum to be an object

---
+++ constants/workoutCategoryEnum.js
@@ -0,0 +1,19 @@
+// Enum representing the types of workouts within a training plan date
+const workoutCategoryEnum = {
+  0: 'Rest',
+  1: 'Long',
+  2: 'Easy',
+  3: 'Tempo',
+  4: 'Intervals',
+  5: 'Repetitions',
+  6: 'Marathon',
+  7: 'Race',
+  8: 'Trail Run',
+  9: 'Hiking',
+  10: 'Spinning',
+  11: 'Downhill Skiing',
+  12: 'Backcountry Skiing',
+  13: 'Lifting',
+}
+
+export default workoutCategoryEnum
