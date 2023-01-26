6c3b49d79f1229a0ae74e89a2bf21ed20b3b0c48
Matthew Bakken <mmbakken@gmail.com>
2023-01-26 13:08:43 -0700

    New workout category name/color mapping

--- constants/workoutCategoryEnum.js
+++ constants/workoutCategoryEnum.js
@@ -1,19 +1,14 @@
 // Enum representing the types of workouts within a training plan date
 const workoutCategoryEnum = {
   0: 'Rest',
-  1: 'Long',
+  1: 'Recovery',
   2: 'Easy',
-  3: 'Tempo',
-  4: 'Intervals',
-  5: 'Repetitions',
-  6: 'Marathon',
+  3: 'Long',
+  4: 'Marathon',
+  5: 'Tempo',
+  6: 'Vo2max',
   7: 'Race',
-  8: 'Trail Run',
-  9: 'Hiking',
-  10: 'Spinning',
-  11: 'Downhill Skiing',
-  12: 'Backcountry Skiing',
-  13: 'Lifting',
+  8: 'Cross Training',
 }
 
 export default workoutCategoryEnum
